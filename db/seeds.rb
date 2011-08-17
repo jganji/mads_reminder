f4fdf9365a86c2ee27e7340a09056d3f045ca55a
Jithendra Ganji <jittu.ganji@gmail.com>
2011-08-16 21:12:28 -0500

    updating data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,11 @@
 
 appointments = Appointment.create([
   {:business_guid => 'guid1', :customer_name => 'Karunakar', 
-  :customer_phone => '1234567890', :appointment_date => '08/31/2011' , :appointment_time => '13:10'},
+  :customer_phone => '1234567891', :appointment_date => '08/31/2011' , :appointment_time => '13:10'},
   {:business_guid => 'guid2', :customer_name => 'Mahesh', 
-  :customer_phone => '1234567890', :appointment_date => '08/30/2011' , :appointment_time => '14:10'},
+  :customer_phone => '1234567892', :appointment_date => '08/30/2011' , :appointment_time => '14:10'},
   {:business_guid => 'guid3', :customer_name => 'Jithu', 
-  :customer_phone => '1234567890', :appointment_date => '08/29/2011' , :appointment_time => '15:10'},
+  :customer_phone => '1234567893', :appointment_date => '08/29/2011' , :appointment_time => '15:10'},
   {:business_guid => 'guid4', :customer_name => 'Madhu', 
-  :customer_phone => '1234567890', :appointment_date => '08/28/2011' , :appointment_time => '16:10'},
+  :customer_phone => '1234567894', :appointment_date => '08/28/2011' , :appointment_time => '16:10'},
   ])
